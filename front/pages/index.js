f4d1418e01d5256cae7aae27b8cff03dedb76b01
pyo <pyohogeun@gmail.com>
2020-01-16 00:27:01 +0900

    antDesign 적용

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
     return (
-        <div>Hello, Next!
-            <ul>
-                <li>
-                    <Link href="/about"><a>about</a></Link>
-                </li>
-            </ul>
-        </div>
+        <>
+            <Head>
+                <title>NodeBird</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.25.3/antd.css" />
+            </Head>
+            <AppLayout>
+                <div>Hello, Next!
+                    <ul>
+                        <li>
+                            <Link href="/about"><a>about</a></Link>
+                        </li>
+                    </ul>
+                </div>
+            </AppLayout>
+        </>
+        
     );
 };
 
