50512fe34fb5bab738ab64d4a001a28507022ee8
pyo <pyohogeun@gmail.com>
2020-01-19 21:30:43 +0900

    로그인

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -8,6 +8,7 @@ const dummy = {
     Post: [],
     Followings: [],
     Followers: [],
+    isLoggedIn: true,
 };
 
 const AppLayout = ({children}) => {
@@ -24,32 +25,36 @@ const AppLayout = ({children}) => {
                 
                 <Row>
                     <Col xs={24} md={6}>
-                        <Card
-                            actions={[
-                                <div key="twit">짹짹<br/>{dummy.Post.length}</div>,
-                                <div key="following">팔로잉 <br/>{dummy.Followings.length}</div>,
-                                <div key="follower"> 팔로워 <br/>{dummy.Followers.length}</div>
-                            ]}
-                        >
-                            <Card.Meta
-                                avatar={<Avatar>{dummy.nickname[0]}</Avatar> }
-                                title={dummy.nickname}
-                            />
-                        </Card>
-                        <Form>
-                            <div>
-                                <label htmlFor="user-id">아이디</label>
-                                <br />
-                                <Input name="user-id" required />
-                            </div>
-                            <div>
-                                <label htmlFor="user-password">비밀번호</label>
-                                <br />
-                                <Input name="user-password" type="password" required />
-                            </div>
-                            <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
-                            <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-                        </Form>
+                        {
+                            dummy.isLoggedIn ?
+                            <Card
+                                actions={[
+                                    <div key="twit">짹짹<br/>{dummy.Post.length}</div>,
+                                    <div key="following">팔로잉 <br/>{dummy.Followings.length}</div>,
+                                    <div key="follower"> 팔로워 <br/>{dummy.Followers.length}</div>
+                                ]}
+                            >
+                                <Card.Meta
+                                    avatar={<Avatar>{dummy.nickname[0]}</Avatar> }
+                                    title={dummy.nickname}
+                                />
+                            </Card>
+                                :
+                            <Form>
+                                <div>
+                                    <label htmlFor="user-id">아이디</label>
+                                    <br />
+                                    <Input name="user-id" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="user-password">비밀번호</label>
+                                    <br />
+                                    <Input name="user-password" type="password" required />
+                                </div>
+                                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                                <Link href="/signup"><a><Button>회원가입</Button></a></Link>
+                            </Form>
+                        }
                     </Col>
                     <Col xs={24} md={12}>
                         {children}
