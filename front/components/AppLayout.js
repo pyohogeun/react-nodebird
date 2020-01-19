79c2ab9073a731b550dad88698d6e7e2480eadc8
pyo <pyohogeun@gmail.com>
2020-01-19 21:06:57 +0900

    로그인 form 추가

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Input, Button, Row, Col, Card, Avatar } from 'antd';
+import { Menu, Input, Button, Row, Col, Card, Avatar, Form } from 'antd';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
@@ -21,7 +21,7 @@ const AppLayout = ({children}) => {
                         <Input.Search enterButton style={{ verticalAlign:'middle'}}/>
                     </Menu.Item>
                 </Menu>
-                <Link href="/signup"><a><Button>회원가입</Button></a></Link>
+                
                 <Row>
                     <Col xs={24} md={6}>
                         <Card
@@ -36,6 +36,20 @@ const AppLayout = ({children}) => {
                                 title={dummy.nickname}
                             />
                         </Card>
+                        <Form>
+                            <div>
+                                <label htmlFor="user-id">아이디</label>
+                                <br />
+                                <Input name="user-id" required />
+                            </div>
+                            <div>
+                                <label htmlFor="user-password">비밀번호</label>
+                                <br />
+                                <Input name="user-password" type="password" required />
+                            </div>
+                            <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                            <Link href="/signup"><a><Button>회원가입</Button></a></Link>
+                        </Form>
                     </Col>
                     <Col xs={24} md={12}>
                         {children}
